c19560265bae4c2d5415b06b042839abce458d40
Ian Zevallos <ian.zevallos.m@gmail.com>
2024-04-22 20:41:44 +0200

    something

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
 import router from './routerConfig.jsx';
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
