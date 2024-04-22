1c56f5bf67ad11dea8c4759f45797173c5e2a802
Ian Zevallos <ian.zevallos.m@gmail.com>
2024-04-22 20:15:06 +0200

    last touch

--- frontend/src/pages/Components/Layout.jsx
+++ frontend/src/pages/Components/Layout.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Sidebar, SidebarItem } from './Sidebar';
-import {BadgePercent , PackageSearch } from "lucide-react";
+import {BadgePercent , PackageSearch, CirclePlus, User  } from "lucide-react";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar>
         <a className="link" href="/products">
+          <SidebarItem icon={<User  size={20}/>} text="Profile" />
+        </a>
+        <a className="link" href="/products">
           <SidebarItem icon={<PackageSearch  size={20}/>} text="Products" />
         </a>
         <a className="link" href="/offers">
           <SidebarItem icon={<BadgePercent  size={20}/>} text="Offers" />
+        </a>
+        <a className="link" href="/offers">
+          <SidebarItem icon={<CirclePlus   size={20}/>} text="CSV" />
         </a>
       </Sidebar>
       <div className="flex-1">
